29f1e77dad0a59654873f97feb91215980351375
Kenji Sagae <sagae@DSS04912-LIN-L.local>
2018-02-09 00:16:10 -0800

    added regular webpage

---
+++ sagae/header.js
@@ -0,0 +1,9 @@
+var cHeader = '<IMG SRC="Kenji_Sagae.jpg" ALT="Kenji Sagae" HEIGHT="170" ALIGN=LEFT> \
+          <h1>Kenji Sagae</h1> \
+          <h2>UC Davis</h2> \
+          <h3>Linguistics</h3> \
+	<h2>sagae<sub><IMG SRC="at.jpg" height=15></sub>ucdavis.edu</h2>\
+'
+document.write(cHeader)
+
+
