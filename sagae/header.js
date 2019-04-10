47fe0769c3a0532f99877f61b3794de1c10b3d00
Kenji Sagae <sagae@users.noreply.github.com>
2019-04-10 16:25:19 -0700

    Update header.js

--- sagae/header.js
+++ sagae/header.js
@@ -1,7 +1,10 @@
 var cHeader = '<IMG SRC="Kenji_Sagae.jpg" ALT="Kenji Sagae" HEIGHT="170" ALIGN=LEFT> \
           <h1>Kenji Sagae</h1> \
-          <h2>UC Davis</h2> \
-          <h3>Linguistics</h3> \
+          <h2>University of California, Davis</h2> \
+          <h3>Director, Linguistics graduate program</h3> \
+          <h3>Vice-chair, Linguistics department</h3> \
+          <h3>Program committee member and affiliated faculty, Cognitive Science program</h3> \
+          <h3>Faculty member, Computer Science graduate group</h3> \
 	<h2>sagae<sub><IMG SRC="at.jpg" height=15></sub>ucdavis.edu</h2>\
 '
 document.write(cHeader)
