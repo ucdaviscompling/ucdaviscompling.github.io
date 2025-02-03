1f99bc7aeb4c1dc7ba594a83d5b3d8657f3d0095
ucdaviscompling <ucdaviscompling@sagae.org>
2025-02-03 12:37:07 -0800

    Update hw4.py

--- LIN127/fallback/hw4.py
+++ LIN127/fallback/hw4.py
@@ -1,2 +1,39 @@
-print("We've exhausted the fallback LLM access option. If you need additional LLM time for your assignment, please contact the instructor.")
+import time
+import os
 
+print("Downloading and installing requirements...")
+os.system('pip install ollama')
+
+time.sleep(3)
+from ollama import Client
+system_msg = ''
+model_name = 'llama3.1:8b'
+client = None
+
+def init(id):
+    global client
+    id = id.strip()
+    client = Client(host='https://'+id+'.ngrok-free.app')
+    print('Initialized')
+
+def gen(user_msg, history=[]):
+    if client == None:
+        print('Initialize first using hw4a.init(id)\n(replace "id" with the ID provided on canvas)')
+        return
+    msgs = [{'role': 'system', 'content': system_msg}]
+    role = 'user'
+    for m in history:
+        msgs.append({'role': role, 'content': m})
+        if role == 'user':
+            role = 'assistant'
+        else:
+            role = 'user'
+    if role != 'user':
+        msgs.pop()
+        role = 'user'
+    msgs.append({'role': role, 'content': user_msg})
+    response = client.chat(model=model_name, messages=msgs)['message']['content']
+
+    return response
+
+print("Ready")
